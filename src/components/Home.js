2af14fa3aedd4c7cbad8e51ea90d393c2a9c9bd3
satyam <satyam@gmail.com>
2023-07-10 12:19:54 +0530

    check

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div>
         <Link to="/adminlogin"><button>Admin</button></Link>
+        <br/>
         <Link to="/managerlogin"><button>Manager</button></Link>
         <Link to="/agentlogin"><button>Agent</button></Link> 
     </div>
